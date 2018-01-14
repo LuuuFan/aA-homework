0a6cd69a06ab530388972851b0ca74be58be8eb9
luuufan <angelia.fan@gmail.com>
2018-01-13 23:42:50 -0800

    Update W6D1

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById('mycanvas');
+  canvas.setAttribute('height', '500');
+  canvas.setAttribute('width', '500');
 
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(100, 100, 100, 100);
+
+  ctx.beginPath();
+  ctx.arc(100, 100, 20, 0, 2*Math.PI, true);
+  ctx.strokeStyle = "gray";
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  ctx.fillStyle = "purple";
+  ctx.fill();
 });
